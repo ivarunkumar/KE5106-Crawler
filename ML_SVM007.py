c1558b3c5f7851e3c7f35af54c6ed2f3704536f5
Wyatt Ho <e0146951@u.nus.edu>
2017-08-25 22:46:13 +0800

    WJ

--- ML_SVM007.py
+++ ML_SVM007.py
@@ -15,46 +15,36 @@ import json
 import csv
 
 ########
-column_names = ['userName','memberSince','ageGroup','gender','homeTown','points','rating','Label']
-df = pd.read_csv('test.csv', delimiter=',', encoding="ISO-8859-1", header=None, sep=',', names=column_names)
 
-for column in df.columns:
-    if df[column].dtype == type(object):
-        le = preprocessing.LabelEncoder()
-        df[column] = le.fit_transform(df[column])
-
-array = df.values
-
-X = array[:,0:7]
-y = array[:,7]
-scaler = MinMaxScaler(feature_range=(0, 1))
-rescaledX = scaler.fit_transform(X)
-# summarize transformed data
-np.set_printoptions(precision=3)
-print(rescaledX[0:7,:])
-
-print("XXXXX:",X)
-print("YYYYYY:",y)	 
 
 ########
-column_names_untested = ['userName','memberSince','ageGroup','gender','homeTown','points','rating']
-df_untested = pd.read_csv('test1.csv', delimiter=',', encoding="ISO-8859-1", header=None, sep=',', names=column_names_untested)
 
-for column1 in df_untested.columns:
-    if df_untested[column1].dtype == type(object):
-        le1 = preprocessing.LabelEncoder()
-        df_untested[column1] = le1.fit_transform(df_untested[column1])
-
-array_untested = df_untested.values
-
-X_untested = array_untested
-scaler = MinMaxScaler(feature_range=(0, 1))
-rescaledX_untested = scaler.fit_transform(X_untested)
-# summarize transformed data
-np.set_printoptions(precision=3)
-print(rescaledX_untested[0:7,:])
 
 ########
+
+def fTrainDataProcess (csv):
+	column_names = ['userName','memberSince','ageGroup','gender','homeTown','points','rating','Label']
+	df = pd.read_csv(csv, delimiter=',', encoding="ISO-8859-1", header=None, sep=',', names=column_names)
+
+	for column in df.columns:
+		if df[column].dtype == type(object):
+			le = preprocessing.LabelEncoder()
+			df[column] = le.fit_transform(df[column])
+
+	array = df.values
+
+	X = array[:,0:7]
+	y = array[:,7]
+	scaler = MinMaxScaler(feature_range=(0, 1))
+	rescaledX = scaler.fit_transform(X)
+	# summarize transformed data
+	np.set_printoptions(precision=3)
+	print(rescaledX[0:7,:])
+
+	print("XXXXX:",rescaledX)
+	print("YYYYYY:",y)	
+
+	fML_SVM_Make_PredModel(rescaledX, y)	
 
 def fML_SVM_Make_PredModel(X, y):    # Model # total attributes + target
 	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.10, random_state=42)
@@ -88,12 +78,20 @@ def fML_SVM_Load_TestModel(i): # Data Set
 #---------------------- Call Functions ----------------------
 
 # Run the function to make a model
-#fML_SVM_Make_PredModel(rescaledX, y)
+#vTrainData_X = [['1','2014','1','1','1','1','3'],
+#['1','2014','1','2','2','2','4'],
+#['1','2014','1','3','3','3','4']]
+
+#vTrainData_Y = ['1','0','0']
+
+#fML_SVM_Make_PredModel(vTrainData_X, vTrainData_Y)
 
 # Run the function to make prediction for untested data
-vTesting = ['20','014','20','20','20','0','2']
-fML_SVM_Load_TestModel(vTesting)
+#vTesting = ['20','014','20','20','20','0','2']
+#fML_SVM_Load_TestModel(vTesting)
 
+#fML_SVM_Load_TestModel(vData)
+fTrainDataProcess("Input_01.csv")
 
 
 
