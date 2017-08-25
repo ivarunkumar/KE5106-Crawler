13b90b4153d071fedd85ee509e48c296f3aeb79a
shanjun26 <31085249+shanjun26@users.noreply.github.com>
2017-08-25 21:03:54 +0800

    update

--- api.py
+++ api.py
@@ -9,38 +9,19 @@ app.config['MONGO_DBNAME'] ='TripAdivor'
 app.config['MONGO_URL']='mongodb://localhost:27017/TripAdvisor'
 
 mongo =PyMongo(app)
-@app.route('/read/<username>')
-def read(username):
+@app.route('/read',methods=['GET'])
+def read():
+    print('----------------------')
+    print(request.args.get('username'))
+    username =request.args.get('username')
 
-    connection = MongoClient('localhost', 27017)  # Connect to mongodb
+    #connection = MongoClient('localhost', 27017)  # Connect to mongodb
+    #print(connection.database_names())  # Return a list of db, equal to: > show dbs
+    #db = connection['TripAdvisor']  # equal to: > use testdb1
+    #print("posts" in db.collection_names())  # Check if collection "posts"
+    # collection = db['Reviewers']
 
-    print(connection.database_names())  # Return a list of db, equal to: > show dbs
-
-    db = connection['TripAdvisor']  # equal to: > use testdb1
-    print("posts" in db.collection_names())  # Check if collection "posts"
-    collection = db['Reviewers']
-
-    ff = collection.count()
     result = runModelForReviewer(username)
 
-    #ff = collection.find({"reviewLocation": "Singapore"})
-   # ff = collection.find({"userName": "780shivs"})
-    print(username)
-   # ff = collection.find({"userName": username })
 
-    data = ''
-    data =ff
-
-    data = [json.dumps(item, default=json_util.default) for item in ff]
     return jsonify(data=result)
-    #return username
-
-@app.route('/test/', methods=['GET'])
-def getQ():
-    print(request.args.get('a'))
-    print(request.args.get('b'))
-    return "lalala"
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
